74b518b6825d89f38f5fa9cbad1ceccc09c71d19
Dronai <pierre.boutin@viacesi.fr>
2021-02-09 20:46:24 +0000

    Create model Assignation

---
+++ reverse/client/assignation.py
@@ -0,0 +1,8 @@
+import reverse.client.MissyCore._model.target import Target
+
+class Assignation():
+
+    def __init__(self, id: int, target: Target, name: String):
+        self.id = id
+        self.target = target
+        self.name = name
