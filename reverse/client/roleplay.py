98ad21e5be30f8834884b5878be05f57f093c2c7
unknown <unknown>
2023-03-16 23:06:56 +0100

    Random role game

---
+++ reverse/client/roleplay.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+ROLES = ["Mage", "Guerrier", "Voleur", "Paladin", "Nécromancien"]
+
+class Roleplayer(commands.Cog):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def rpjoin(self, ctx, role):
+		# Ajouter un rôle au membre qui a envoyé la commande
+		if role in ROLES:
+			guild = ctx.guild
+			member = ctx.author
+			role_obj = discord.utils.get(guild.roles, name=role)
+			await member.add_roles(role_obj)
+			await ctx.send(f"{member.mention} a rejoint la guilde des {role}s !")
+		else:
+			await ctx.send(f"{role} n'est pas un rôle valide.")
+
+	@commands.command()
+	async def rpleave(self, ctx, role):
+		# Supprimer un rôle du membre qui a envoyé la commande
+		if role in ROLES:
+			guild = ctx.guild
+			member = ctx.author
+			role_obj = discord.utils.get(guild.roles, name=role)
+			await member.remove_roles(role_obj)
+			await ctx.send(f"{member.mention} a quitté la guilde des {role}s.")
+		else:
+			await ctx.send(f"{role} n'est pas un rôle valide.")
+
+async def setup(bot):
+	await bot.add_cog(Roleplayer(bot))
