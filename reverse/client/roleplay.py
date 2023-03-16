f1a355297aa6547366fdc7a8ec7bec49c1e16867
unknown <unknown>
2023-03-16 23:26:10 +0100

    debug

--- reverse/client/roleplay.py
+++ reverse/client/roleplay.py
@@ -12,6 +12,7 @@ class Roleplayer(commands.Cog):
 	async def rpjoin(self, ctx, role):
 		# Ajouter un rôle au membre qui a envoyé la commande
 		print(role)
+		print(role in ROLES)
 		if role in ROLES:
 			guild = ctx.guild
 			member = ctx.author
@@ -25,6 +26,7 @@ class Roleplayer(commands.Cog):
 	async def rpleave(self, ctx, role):
 		# Supprimer un rôle du membre qui a envoyé la commande
 		print(role)
+		print(role in ROLES)
 		if role in ROLES:
 			guild = ctx.guild
 			member = ctx.author
